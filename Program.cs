c51d697216ddfca7a4cf4b6f39822acbe13077c8
Russ <iamkrashenbern@yahoo.com>
2023-04-18 13:29:53 -0500

    Initialize

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QUEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
